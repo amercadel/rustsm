bf484fda8fb467e7657afe0d0f0bd3682aa46118
Alex Mercadel <alexmercadel@Alexs-MacBook-Pro.local>
2026-04-27 19:47:01 -0500

    create repo

---
+++ target/debug/deps/rustsm-d9f8010d68f7fe81.d
@@ -0,0 +1,5 @@
+/Users/alexmercadel/Documents/projects/active/rustsm/target/debug/deps/librustsm-d9f8010d68f7fe81.rmeta: src/main.rs
+
+/Users/alexmercadel/Documents/projects/active/rustsm/target/debug/deps/rustsm-d9f8010d68f7fe81.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rustsm-f104cc531d2e3b7e.d
@@ -0,0 +1,5 @@
+/Users/alexmercadel/Documents/projects/active/rustsm/target/debug/deps/librustsm-f104cc531d2e3b7e.rmeta: src/main.rs
+
+/Users/alexmercadel/Documents/projects/active/rustsm/target/debug/deps/rustsm-f104cc531d2e3b7e.d: src/main.rs
+
+src/main.rs:
